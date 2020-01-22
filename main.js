89a8fb67aa81814ba9497f874a61d6a6b23520f1
Blissfulair <blessing.airehenbuwa@gmail.com>
2020-01-22 08:29:32 +0100

    first commit

--- main.js
+++ main.js
@@ -17,13 +17,39 @@ let themes = {
         'black': '#000000',
         'bg': ' #F6F6F6',
         'header':'#000000',
-        'content':'#000000'
+        'content':'#000000',
+        'main':'#000000',
+        'dark-border':'#000000'
     },
     dark:{
         'black': '#000000',
         'bg': ' #000000',
         'header':'transparent',
-        'content':'#F7FFF'
+        'content':'#F7FFFF',
+        'main':'#fff',
+        'dark-border':'#707070'
+    }
+  }
+  function toggleTheme(){
+    const theme = localStorage.getItem('theme');
+    if(JSON.parse(theme)){
+      setTheme('dark');
+      localStorage.removeItem('theme');
+      document.querySelector('.slide').style.justifyContent ='end'
+    }else{
+      setTheme('light');
+      localStorage.setItem('theme',true);
+      document.querySelector('.slide').style.justifyContent ='start'
+    }
+  }
+  function position(){
+    const theme = localStorage.getItem('theme');
+    if(JSON.parse(theme)){
+      setTheme('light');
+      document.querySelector('.slide').style.justifyContent ='start'
+    }else{
+      setTheme('dark');
+      document.querySelector('.slide').style.justifyContent ='end'
     }
   }
 function setTheme(currentTheme){
@@ -36,8 +62,10 @@ function setTheme(currentTheme){
         document.body.style.setProperty(cssKey, cssValue);
       })
 }
+
 window.addEventListener('load',()=>{
   registerSW();
+  position();
 })
 async function registerSW(){
   if('serviceWorker' in navigator){
